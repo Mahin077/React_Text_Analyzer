6c1b64beed34878985797b8dd20d1f7f0712fece
Mahin077 <mmahinm10@gmail.com>
2023-08-23 01:09:02 +0600

    uppercase lowercase added

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     <Router>
     <Nav/>
     <Routes>
-      <Route exact path='/' element={<Home operationName={"wordCounter"}/>}/>
-      <Route exact path='/uppercase' element={<Home operationName={"uppercase"}/>}/>
-      <Route exact path='/lowercase' element={<Home operationName={"lowercase"}/>}/>
+      <Route exact path='/' element={<Home operationName={"wordCounter"} />}/>
+      <Route exact path='/uppercase' element={<Home operationName={"uppercase"}btName={"Upper Case"}/>}/>
+      <Route exact path='/lowercase' element={<Home operationName={"lowercase"} btName={"Lower Case"}/>}/>
       <Route exact path='/removeextraspace' element={<Home operationName={"removeExtraSpace"}/>}/>
       <Route exact path='/replacewords' element={<Home operationName={"replaceWords"}/>}/>
       <Route exact path='/convertwords' element={<Home operationName={"convertWords"}/>}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -18,31 +18,45 @@ function Home(props) {
     const clearButton = () => {
         setText('')
     }
+    const opertaionButton = () => {
+        //uppercase button 
+        if (props.operationName === "uppercase") {
+            let newText = text.toUpperCase();
+            setText(newText);
+        }
+        //lowercase button
+        else if (props.operationName === "lowercase") {
+            let newText = text.toLowerCase();
+            setText(newText);
+        }
+        
+    }
 
     return (
         <>
-        <div className="card">
-            <div className="card-body">
-                <div className="container">
+            <div className="card">
+                <div className="card-body">
+                    <div className="container">
 
-                    <h3 className='my-3 text-center'>You can find out total words and characters of your given text</h3>
-                    <div className="my-3">
-                        <textarea className="form-control" onChange={textAreaChange} value={text} rows={15} placeholder='Write something'></textarea>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                        <button className={`btn btn-primary`} onClick={listenButton} style={{ marginLeft: 7 }} ><i className="fa-solid fa-volume-high"></i><span style={{ marginLeft: 7 }}>Listen</span></button>
-                        <button className={`btn btn-primary`} onClick={clearButton} style={{ marginRight: 7 }} ><i className="fa-solid fa-trash"></i><span style={{ marginLeft: 7 }}>Clear</span></button>
+                        <h3 className='my-3 text-center'>You can find out total words and characters of your given text</h3>
+                        <div className="my-3">
+                            <textarea className="form-control" onChange={textAreaChange} value={text} rows={15} placeholder='Write something'></textarea>
+                        </div>
+                        <div className="d-flex justify-content-between">
+                            {props.operationName === "wordCounter" && (<button className={`btn btn-primary`} onClick={listenButton} style={{ marginLeft: 7 }} ><i className="fa-solid fa-volume-high"></i><span style={{ marginLeft: 7 }}>Listen</span></button>)}
+                            {props.operationName !== "wordCounter" && (<button className={`btn btn-primary`} onClick={opertaionButton} style={{ marginLeft: 7 }} ><i class="fa-solid fa-gears"></i><span style={{ marginLeft: 7 }}>{props.btName}</span></button>)}
+                            <button className={`btn btn-primary`} onClick={clearButton} style={{ marginRight: 7 }} ><i className="fa-solid fa-trash"></i><span style={{ marginLeft: 7 }}>Clear</span></button>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-            {props.operationName==="wordCounter" && (
-            <div className="container my-5">
-                <h1>Text summary</h1>
-                <p style={{ fontSize: 20, marginLeft: 5 }}>There are {text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.replace(/\s/g, '').length} characters</p>
-            </div>
+            {props.operationName === "wordCounter" && (
+                <div className="container my-5">
+                    <h1>Text summary</h1>
+                    <p style={{ fontSize: 20, marginLeft: 5 }}>There are {text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.replace(/\s/g, '').length} characters</p>
+                </div>
             )}
         </>
     )
